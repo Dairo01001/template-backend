96c7af2769ccd07f6ffd746c2af1a0d1b5f8b263
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2024-08-23 10:38:37 -0500

    add pino and refactor

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Application } from 'express';
 import { PORT } from './config';
 import { unkanowEndpoint } from './middlewares';
+import morgan from 'morgan';
+import routes from './routes';
 
 const createApp = () => {
   const app: Application = express();
   app.set('port', PORT);
+  app.disable('x-powered-by');
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan('dev'));
 
-  app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-  });
-
+  routes(app);
   app.use(unkanowEndpoint);
 
   return app;
